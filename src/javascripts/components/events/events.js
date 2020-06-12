79a14686a4df9d13c92d156444ba302b8c88a843
Luke Lancaster <luketlancaster@gmail.com>
2020-06-12 13:09:36 -0500

    Fun/play component works

--- src/javascripts/components/events/events.js
+++ src/javascripts/components/events/events.js
@@ -1,12 +1,19 @@
 import eat from '../eat/eat';
+import play from '../play/play';
 
 const eatEvents = () => {
   $('#eat').on('click', '#eat-big', eat.eatBig);
   $('#eat').on('click', '#eat-small', eat.eatSmall);
 };
 
+const playEvents = () => {
+  $('#play').on('click', '#fun-big', play.funBig);
+  $('#play').on('click', '#fun-small', play.funSmall);
+};
+
 const allEvents = () => {
   eatEvents();
+  playEvents();
 };
 
 export default { allEvents };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,12 @@
 import '../styles/main.scss';
 import eat from './components/eat/eat';
 import events from './components/events/events';
+import play from './components/play/play';
 
 const init = () => {
   events.allEvents();
   eat.makeEatComponent();
+  play.makeFunComponent();
 };
 
 init();
